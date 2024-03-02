7bea3aa505e535677770d3262caffbbc086e44a0
Jorge Calvi <goalver@hotmail.com>
2024-03-01 21:24:58 -0300

    fixing ERROR 3

--- w01-task.js
+++ w01-task.js
@@ -16,9 +16,9 @@ console.log(`The total is ${theTotal}`);
 
 // 'total' function declaration
 function total(...theNumbers) {
-	let sum = 0;
-	for (let aNumber in theNumbers) {
-		sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
-	}
-	return sum
+    let sum = 0;
+    for (let aNumber of theNumbers) {
+        sum += aNumber * 1; // Implicitly converts a string to a number.
+    }
+    return sum;
 }
